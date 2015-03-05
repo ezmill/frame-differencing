ffde8bc3099f70d181e7f67275abb2194d280b46
Ezra Miller <emiller@lsoc.org>
2015-03-05 16:59:49 -0600

    test

---
+++ main.js
@@ -0,0 +1,121 @@
+var scene, camera, renderer, controls;
+var container;
+var loader;
+var w = window.innerWidth;
+var h = window.innerHeight;
+var mouseX, mouseY;
+var globalUniforms;
+var time = 0;
+var video, videoLoaded = false, camTex;
+var scene1, scene2;
+var rt1, rt2;
+var material1, material2;
+var planeGeometry;
+var mesh1, mesh2;
+initScene();
+function initScene(){
+	container = document.createElement('div');
+    document.body.appendChild(container);
+
+    camera = new THREE.PerspectiveCamera(50, w / h, 1, 100000);
+    camera.position.set(0,0, 750);//test
+    cameraRTT = new THREE.OrthographicCamera( w / - 2, w / 2, h / 2, h / - 2, -10000, 10000 );
+	cameraRTT.position.z = 100;
+
+	controls = new THREE.OrbitControls(camera);
+
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(w, h);
+    renderer.setClearColor(0xffffff, 1);
+    container.appendChild(renderer.domElement);
+
+    scene = new THREE.Scene();
+
+    initCameraTex();
+    animate();
+}
+
+function initCameraTex(){
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+    if (navigator.getUserMedia) {       
+        navigator.getUserMedia({video: true, audio: false}, function(stream){
+        	var url = window.URL || window.webkitURL;
+			video = document.createElement("video");
+	        video.src = url ? url.createObjectURL(stream) : stream;
+	        video.play();
+	        videoLoaded = true;
+	        tex = new THREE.Texture(video);
+	        tex.needsUpdate = true;
+	        camTex = tex;
+	        initFrameDifferencing();
+        }, function(error){
+		   console.log("Failed to get a stream due to", error);
+	    });
+	}
+}
+
+function initFrameDifferencing(){
+	console.log("what");
+	planeGeometry = new THREE.PlaneBufferGeometry(w,h);
+	dummyTex = new THREE.ImageUtils.loadTexture("img/test.png")
+
+	scene1 = new THREE.Scene();
+	rt1 = new THREE.WebGLRenderTarget(w, h, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter, format: THREE.RGBFormat });
+	material1 = new THREE.ShaderMaterial({
+		uniforms: {
+			time: { type: 'f' , value: time},
+			resolution: {type: 'v2', value: new THREE.Vector2(w,h)},
+			texture: {type: 't', value: camTex}
+		},
+		vertexShader: document.getElementById("vs").textContent,
+		fragmentShader: document.getElementById("fs").textContent
+	});
+	mesh1 = new THREE.Mesh(planeGeometry, material1);
+	mesh1.position.set(0, 0, 0);
+	scene1.add(mesh1);
+
+	scene2 = new THREE.Scene();
+	rt2 = new THREE.WebGLRenderTarget(w, h, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter, format: THREE.RGBFormat });
+	material2 = new THREE.ShaderMaterial({
+		uniforms: {
+			time: { type: 'f' , value: time},
+			resolution: {type: 'v2', value: new THREE.Vector2(w,h)},
+			texture: {type: 't', value: rt1}
+		},
+		vertexShader: document.getElementById("vs").textContent,
+		fragmentShader: document.getElementById("fs").textContent
+	});
+	mesh2 = new THREE.Mesh(planeGeometry, material2);
+	mesh2.position.set(0, 0, 0);
+	scene2.add(mesh2);
+
+	material = new THREE.ShaderMaterial({
+		uniforms: {
+			time: { type: 'f' , value: time},
+			resolution: {type: 'v2', value: new THREE.Vector2(w,h)},
+			texture: {type: 't', value: rt1},
+			texture2: {type: 't', value: rt2},
+			texture3: {type: 't', value: camTex} 
+		},
+		vertexShader: document.getElementById("vs").textContent,
+		fragmentShader: document.getElementById("diffFs").textContent
+	});
+	mesh = new THREE.Mesh(planeGeometry, material);
+	scene.add(mesh);
+}
+function animate(){
+	window.requestAnimationFrame(animate);
+	draw();
+}
+
+function draw(){
+    camTex.needsUpdate = true;
+
+    renderer.render(scene1, cameraRTT, rt1, true);
+
+	renderer.render(scene, camera);
+
+	renderer.render(scene2, cameraRTT, rt2, true);
+
+}
