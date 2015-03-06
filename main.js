7d6efa5e37f9a07347ad3564412f8e294ef50827
Ezra Miller <emiller@lsoc.org>
2015-03-06 14:30:19 -0600

    dope

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ var rt1, rt2;
 var material1, material2;
 var planeGeometry;
 var mesh1, mesh2;
+var time = 0.0;
 initScene();
 function initScene(){
 	container = document.createElement('div');
@@ -56,7 +57,6 @@ function initCameraTex(){
 }
 
 function initFrameDifferencing(){
-	console.log("what");
 	planeGeometry = new THREE.PlaneBufferGeometry(w,h);
 	dummyTex = new THREE.ImageUtils.loadTexture("img/test.png")
 
@@ -69,7 +69,7 @@ function initFrameDifferencing(){
 			texture: {type: 't', value: camTex}
 		},
 		vertexShader: document.getElementById("vs").textContent,
-		fragmentShader: document.getElementById("fs").textContent
+		fragmentShader: document.getElementById("flowFs").textContent
 	});
 	mesh1 = new THREE.Mesh(planeGeometry, material1);
 	mesh1.position.set(0, 0, 0);
@@ -81,16 +81,19 @@ function initFrameDifferencing(){
 		uniforms: {
 			time: { type: 'f' , value: time},
 			resolution: {type: 'v2', value: new THREE.Vector2(w,h)},
-			texture: {type: 't', value: rt1}
+			texture: {type: 't', value: rt1},
+			texture2: {type: 't', value: camTex}
 		},
 		vertexShader: document.getElementById("vs").textContent,
-		fragmentShader: document.getElementById("fs").textContent
+		fragmentShader: document.getElementById("fbFs").textContent
 	});
 	mesh2 = new THREE.Mesh(planeGeometry, material2);
 	mesh2.position.set(0, 0, 0);
 	scene2.add(mesh2);
 
-	material = new THREE.ShaderMaterial({
+	sceneDiff = new THREE.Scene();
+	rtDiff = new THREE.WebGLRenderTarget(w, h, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter, format: THREE.RGBFormat });
+	materialDiff = new THREE.ShaderMaterial({
 		uniforms: {
 			time: { type: 'f' , value: time},
 			resolution: {type: 'v2', value: new THREE.Vector2(w,h)},
@@ -101,8 +104,41 @@ function initFrameDifferencing(){
 		vertexShader: document.getElementById("vs").textContent,
 		fragmentShader: document.getElementById("diffFs").textContent
 	});
+	meshDiff = new THREE.Mesh(planeGeometry, materialDiff);
+	sceneDiff.add(meshDiff);
+
+	sceneFB = new THREE.Scene();
+	rtFB = new THREE.WebGLRenderTarget(w, h, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter, format: THREE.RGBFormat });
+	materialFB = new THREE.ShaderMaterial({
+		uniforms: {
+			time: { type: 'f' , value: time},
+			resolution: {type: 'v2', value: new THREE.Vector2(w,h)},
+			texture: {type: 't', value: rtDiff}
+		},
+		vertexShader: document.getElementById("vs").textContent,
+		fragmentShader: document.getElementById("fbFs").textContent
+	});
+	meshFB = new THREE.Mesh(planeGeometry, materialFB);
+	sceneFB.add(meshFB);
+
+	sceneFB2 = new THREE.Scene();
+	rtFB2 = new THREE.WebGLRenderTarget(w, h, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter, format: THREE.RGBFormat });
+	materialFB2 = new THREE.ShaderMaterial({
+		uniforms: {
+			time: { type: 'f' , value: time},
+			resolution: {type: 'v2', value: new THREE.Vector2(w,h)},
+			texture: {type: 't', value: rtFB}
+		},
+		vertexShader: document.getElementById("vs").textContent,
+		fragmentShader: document.getElementById("flowFs").textContent
+	});
+	meshFB2 = new THREE.Mesh(planeGeometry, materialFB2);
+	sceneFB2.add(meshFB2);
+
+	material = new THREE.MeshBasicMaterial({map: rtFB2});
 	mesh = new THREE.Mesh(planeGeometry, material);
 	scene.add(mesh);
+
 }
 function animate(){
 	window.requestAnimationFrame(animate);
@@ -110,12 +146,32 @@ function animate(){
 }
 
 function draw(){
+	time+=0.01;
     camTex.needsUpdate = true;
 
-    renderer.render(scene1, cameraRTT, rt1, true);
-
-	renderer.render(scene, camera);
+    expand(1.01);
+    // materialDiff.uniforms.texture.value = rtFB;
+    material1.uniforms.texture.value = rtDiff;
+    // material2.uniforms.texture.value = rtFB;
 
 	renderer.render(scene2, cameraRTT, rt2, true);
 
+	renderer.render(sceneDiff, cameraRTT, rtDiff, true);
+
+	renderer.render(sceneFB, cameraRTT, rtFB, true);
+	renderer.render(sceneFB2, cameraRTT, rtFB2, true);
+
+	renderer.render(scene, camera);
+
+    renderer.render(scene1, cameraRTT, rt1, true);
+
+
+    var a = rtFB;
+    rtFB = rt1;
+    rt1 = a;
+
+}
+
+function expand(expand){
+		meshDiff.scale.set(expand,expand,expand);
 }
